327a412b55e314b43e41d45da4161ee3c15d579e
Ly <Ly@qq.com>
2022-06-06 22:14:25 +0800

    hot-fix-second-commit

--- src/main/java/com/ly/GitTest.java
+++ src/main/java/com/ly/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一个git");
         System.out.println("第一个git2");
-        System.out.println("第一个git3");
+        System.out.println("第一个git2");
+        System.out.println("hot-fix");
     }
 }
