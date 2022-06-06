a897d88d08668b935cf87c36406dad24d601f6f2
Ly <Ly@qq.com>
2022-06-06 22:12:04 +0800

    hot-fix-first-commit

--- src/main/java/com/ly/GitTest.java
+++ src/main/java/com/ly/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一个git");
         System.out.println("第一个git2");
+        System.out.println("第一个git3");
     }
 }
